7c87843f74ffccbedfdbbdedfce05a7a6e946155
wang465745776 <465745776@qq.com>
2018-03-27 20:05:19 +0800

    1. add custom banner 2. change application name 3. add grobal properties

---
+++ src/main/java/com/wanggt/freedom/springboot/study/StudyApplication.java
@@ -0,0 +1,21 @@
+package com.wanggt.freedom.springboot.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class StudyApplication {
+	
+	@RequestMapping("/helloWorld")
+	private String helloWorld() {
+		return "Hello World";
+	}
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyApplication.class, args);
+    }
+
+}
